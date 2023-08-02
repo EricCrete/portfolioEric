bef9b9367be8eaca616728be45c4b666a11e0cba
Eric Crete <cireeterc@hotmail.com>
2023-08-02 19:31:48 -0400

    realFinal

--- components/TemoignageListe.jsx
+++ components/TemoignageListe.jsx
@@ -1,2 +1,21 @@
+import React from 'react';
+import styles from '../styles/Project.module.css';
+import { useSelector } from 'react-redux';
+
+const TemoignagesListe = () => {
+  const temoignages = useSelector((state) => state.temoignages);
+
+  return (
+    <div className={styles.container}>
+      <h1 className={styles.title}>Liste des témoignages</h1>
+      {temoignages.map((temoignage, index) => (
+        <div key={index}>
+          <p className={styles.description}>{temoignage.message}</p>
+          <p className={styles.description}> Par : {temoignage.auteur}</p>
+        </div>
+      ))}
+    </div>
+  );
+};
 
 export default TemoignagesListe;
